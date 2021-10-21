19463bbf967826bc0f6fc3860f106315250c3ff2
Alex Ozdemir <aozdemir@hmc.edu>
2021-10-20 20:15:28 -0700

    llbp tests for pasta and vellas

--- src/proofs/llbp.rs
+++ src/proofs/llbp.rs
@@ -1,5 +1,5 @@
 use crate::{
-    curves::TEPair,
+    curves::TwoChain,
     reductions::combinator::{RepeatWhile, Sequence, TrueReductionToProof},
     reductions::{
         bp_2ary_step::Bp2aryStep, bp_unroll_to_com_r1cs::UnrollToComR1cs,
@@ -8,8 +8,6 @@ use crate::{
     relations::ipa::{IpaInstance, IpaRelation},
     Proof,
 };
-use ark_ec::{twisted_edwards_extended::GroupProjective, ModelParameters};
-use ark_ff::PrimeField;
 
 /// Build an IPA protocol which
 /// * does `r` rounds of committed `k`-ary recursion,
@@ -18,17 +16,15 @@ use ark_ff::PrimeField;
 /// * proves that via the classic bulletproofs.
 ///
 /// Defined using [crate::Reduction] combinators.
-pub fn llbp<P: TEPair>(k: usize, r: usize) -> impl Proof<IpaRelation<GroupProjective<P::P1>>>
-where
-    <P::P1 as ModelParameters>::BaseField: PrimeField,
+pub fn llbp<C: TwoChain>(k: usize, r: usize) -> impl Proof<IpaRelation<C::G1>>
 {
     TrueReductionToProof::new(Sequence::new(
         Sequence::new(
-            Sequence::new(IpaToBpUnroll::<P>::new(k, r), UnrollToComR1cs::default()),
+            Sequence::new(IpaToBpUnroll::<C>::new(k, r), UnrollToComR1cs::default()),
             ComR1csToIpa::default(),
         ),
         Sequence::new(
-            RepeatWhile::new(Bp2aryStep::default(), |x: &IpaInstance<P::G2>| {
+            RepeatWhile::new(Bp2aryStep::default(), |x: &IpaInstance<C::G2>| {
                 x.gens.vec_size > 1
             }),
             SendIpa::default(),
@@ -39,11 +35,23 @@ where
 #[cfg(test)]
 mod test {
     use super::llbp;
-    use crate::{curves::models::JubJubPair, test::test_ipa};
+    use crate::{curves::models::{JubJubPair, VellasPair, PastaPair}, test::test_ipa};
     #[test]
     #[ignore]
-    fn test_bls() {
+    fn test_jubjub() {
         let bp_pf = llbp::<JubJubPair>(8, 2);
+        test_ipa(vec![1024], 1, bp_pf);
+    }
+    #[test]
+    #[ignore]
+    fn test_pasta() {
+        let bp_pf = llbp::<PastaPair>(8, 2);
+        test_ipa(vec![1024], 1, bp_pf);
+    }
+    #[test]
+    #[ignore]
+    fn test_vellas() {
+        let bp_pf = llbp::<VellasPair>(8, 2);
         test_ipa(vec![1024], 1, bp_pf);
     }
 }
