35face95f2967a883ca15b249f8c393f02ec5f49
Alex Ozdemir <aozdemir@hmc.edu>
2021-11-11 15:13:00 -0800

    single plot

--- analysis/main/plot.R
+++ analysis/main/plot.R
@@ -1,4 +1,7 @@
 library(tidyverse)
+library(ggpubr)
+library(stringr)
+library(scales)
 
 d <- read_csv("data.csv")
 
@@ -11,31 +14,45 @@ sum_d <- d %>%
               ver_time=mean(ver_wall_time))
 print(sum_d)
 
-ggplot(sum_d, aes(x=n, y=pf_size, color=pf)) +
-    geom_point() +
-    scale_x_continuous(trans="log2") +
-    labs(x = "IPA Size",
-         y = "Proof Size (elements)",
-         title = "Proof Sizes"
-         )
-ggsave("pf_size.pdf", units = "in", width = 4, height = 3)
+xlog2 <- scale_x_continuous(trans = log2_trans(),
+                           breaks = trans_breaks("log2", function(x) 2^x),
+                           labels = trans_format("log2", math_format(2^.x)))
+ylog2 <- scale_y_continuous(trans = log2_trans(),
+                           breaks = trans_breaks("log2", function(x) 2^x),
+                           labels = trans_format("log2", math_format(2^.x)))
 
-ggplot(sum_d, aes(x=n, y=pf_time, color=pf)) +
-    geom_point() +
-    scale_x_continuous(trans="log2") +
-    scale_y_continuous(trans="log2") +
-    labs(x = "IPA Size",
-         y = "Proving Time (s)",
-         title = "Proving Times"
-         )
-ggsave("pf_time.pdf", units = "in", width = 4, height = 3)
-
-ggplot(sum_d, aes(x=n, y=ver_time, color=pf)) +
-    geom_point() +
-    scale_x_continuous(trans="log2") +
-    scale_y_continuous(trans="log2") +
-    labs(x = "IPA Size",
-         y = "Verification Time (s)",
-         title = "Verification Times"
-         )
-ggsave("ver_time.pdf", units = "in", width = 4, height = 3)
+ggarrange(
+    ggplot(sum_d, aes(x=n, y=pf_size, color=pf, shape=pf)) +
+        geom_point() +
+        xlog2 +
+        labs(x = "IPA Size",
+             y = "Proof Size (elements)",
+             title = "Proof Size",
+             color = "Protocol",
+             shape = "Protocol"
+             ),
+    ggplot(sum_d, aes(x=n, y=pf_time, color=pf, shape=pf)) +
+        geom_point() +
+        xlog2 +
+        ylog2 +
+        labs(x = "IPA Size",
+             y = "Proving Time (s)",
+             title = "Proving Time",
+             color = "Protocol",
+             shape = "Protocol"
+             ),
+    ggplot(sum_d, aes(x=n, y=ver_time, color=pf, shape=pf)) +
+        geom_point() +
+        xlog2 +
+        ylog2 +
+        labs(x = "IPA Size",
+             y = "Verification Time (s)",
+             title = "Verification Time",
+             color = "Protocol",
+             shape = "Protocol"
+             ),
+    ncol=3,
+    nrow=1,
+    common.legend=TRUE,
+    legend="right")
+ggsave("main_plot.pdf", units = "in", width = 6, height = 3)
